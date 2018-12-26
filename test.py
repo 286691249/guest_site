29caacce5542075b319445bfb800e271e313fed9
xianglin <286691249@qq.com>
2018-12-26 20:40:28 +0800

    test push

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+class test_print_helloworld():
+    def HelloWord():
+        print('hello world')
+
+if __name__=='__main__':
+    test_print_helloworld.HelloWord()
