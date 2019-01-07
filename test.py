cd0547be1fba7487f41e34e6e22ac41d34f7b88d
286691249 <37439896+286691249@users.noreply.github.com>
2019-01-08 00:41:00 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-class test_print_helloworld():
-    def HelloWord():
-        print('hello world')
-
-if __name__=='__main__':
-    test_print_helloworld.HelloWord()
